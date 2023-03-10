d21db80750eecc03cf35bba8e2dedd27c1a630e1
Sagar Shiroya <sagar.shiroya8@gmail.com>
2023-03-10 17:35:12 +0530

    Changes related to clicking on different squares and displaying  x and o in it

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import "./App.css";
 import { Footer } from "./Footer";
+import { Board } from "./Board";
 
 function App() {
   return (
     <div className="container">
-      <h1>Tic-Tac-Toe Game</h1>
+      <h1>Tic-Tac-Toe</h1>
       <Board />
       <Footer />
     </div>
   );
 }
-
-const Board = () => {
-  return (
-    <>
-      <div className="board-row">
-        <button className="square"> 1</button>
-        <button className="square"> 2</button>
-        <button className="square"> 3</button>
-      </div>
-      <div className="board-row">
-        <button className="square"> 4</button>
-        <button className="square"> 5</button>
-        <button className="square"> 6</button>
-      </div>
-      <div className="board-row">
-        <button className="square"> 7</button>
-        <button className="square"> 8</button>
-        <button className="square"> 9</button>
-      </div>
-    </>
-  );
-};
 
 export default App;
